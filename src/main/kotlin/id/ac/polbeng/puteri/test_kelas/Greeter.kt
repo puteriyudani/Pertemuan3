6052dc9c0d8e46ee55d1a5c2b6d05dbde8267042
Puteri Yudani <puteriyudani@gmail.com>
2022-03-25 10:56:52 +0700

    Mengubah kode program contoh penggunaan kelas pada Kotlin

--- src/main/kotlin/id/ac/polbeng/puteri/test_kelas/Greeter.kt
+++ src/main/kotlin/id/ac/polbeng/puteri/test_kelas/Greeter.kt
@@ -1,7 +1,16 @@
 package id.ac.polbeng.puteri.test_kelas
 
-class Greeter { //kelas greeter
+class Greeter {
+    var text: String = "" //atribut text
+
+    //dua buah method greet
     fun greet() {
         println("Hello object world!")
+    }
+    fun greet(name: String) {
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String { //nilai kembali string
+        return "$text $name"
     }
 }
--- src/main/kotlin/id/ac/polbeng/puteri/test_kelas/GreeterTest.kt
+++ src/main/kotlin/id/ac/polbeng/puteri/test_kelas/GreeterTest.kt
@@ -1,7 +1,16 @@
 package id.ac.polbeng.puteri.test_kelas
 
 fun main() {
-    //inisiasi kelas greeter
     val greeter = Greeter()
     greeter.greet()
+    greeter.text = "Hi" //atribut text bernilai "Hi"
+
+    //pemanggilan method greet dari kelas Greeter
+    greeter.greet("Puteri")
+    greeter.greet("Yudani")
+
+    greeter.text = "Hello programmer" //menambahkan nilai text dengan "Hello programmer"
+
+    //pemanggilan fungsi with_ret_val dengan parameter string
+    println(greeter.with_ret_val("Puput"))
 }
