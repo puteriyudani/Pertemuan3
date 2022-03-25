89ca66ce516d1cca63a348f76f6c55aa64370892
Puteri Yudani <puteriyudani@gmail.com>
2022-03-25 10:47:11 +0700

    Percobaan contoh penggunaan kelas di Kotlin

---
+++ src/main/kotlin/id/ac/polbeng/puteri/test_kelas/Greeter.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.puteri.test_kelas
+
+class Greeter { //kelas greeter
+    fun greet() {
+        println("Hello object world!")
+    }
+}
---
+++ src/main/kotlin/id/ac/polbeng/puteri/test_kelas/GreeterTest.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.puteri.test_kelas
+
+fun main() {
+    //inisiasi kelas greeter
+    val greeter = Greeter()
+    greeter.greet()
+}
