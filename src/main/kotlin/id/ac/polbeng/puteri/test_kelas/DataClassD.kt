60451540db2707c31dae99936dbf726f31f9438f
Puteri Yudani <puteriyudani@gmail.com>
2022-03-29 16:29:41 +0700

    Percobaan Data class toString() method

---
+++ src/main/kotlin/id/ac/polbeng/puteri/test_kelas/DataClassD.kt
@@ -0,0 +1,6 @@
+package id.ac.polbeng.puteri.test_kelas
+//fungsi toString() mengembalikan nilai kembalian string
+fun main(){
+    val meri = Student("Meri", 20)
+    println(meri.toString()) //fungsi toString() untuk objek meri
+}
